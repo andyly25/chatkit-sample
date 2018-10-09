a634b65d2037f42f51596e82b6c84c81874b3af6
Andy Ly <andywindsong@gmail.com>
2018-10-08 17:26:55 -0700

    add username component

--- server.js
+++ server.js
@@ -1,12 +1,38 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const cors = require('cors')
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+// import chatkit
+const ChatKit = require('pusher-chatkit-server');
 
 const app = express()
+
+// initiate our own chatkit
+const chatkit = new ChatKit.default({
+  instanceLocator: 'v1:us1:5dff3a4f-a6e4-4036-b974-d09e53dc0568',
+  key: '8735902e-5985-4858-a619-7f8ecb5dea3c:KHi93UxzBwxTdqbUUrFzk51UTDktYKfvVBOsjai2bEI='
+});
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 app.use(cors())
+
+// we then take a user and create a chatkit user through chatkit instance
+app.post('/users', (req, res) => {
+  const { username } = req.body;
+  chatkit
+    .createUser({
+      id: username,
+      name: username
+    })
+    .then(() => res.sendStatus(201))
+    .catch(error => {
+      if (error.error_type === 'services/chatkit/user_already_exists') {
+        res.sendStatus(200);
+      } else {
+        res.status(error.status).json(error);
+      }
+    });
+});
 
 const PORT = 3001
 app.listen(PORT, err => {
--- src/App.js
+++ src/App.js
@@ -1,8 +1,36 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+// import username component
+import UsernameForm from './components/UsernameForm';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      currentUsername: ''
+    }
+    this.onUsernameSubmitted = this.onUsernameSubmitted.bind(this);
+  }
+
+  onUsernameSubmitted(username) {
+    fetch('http://localhost:3001/users', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ username })
+    })
+      .then(response => {
+        this.setState({
+          currentUsername: username
+        })
+      })
+      .catch(error => console.error('error', error));
+  }
+
   render() {
-    return <h1>Chatly</h1>
+    // render UsernameForm and hook up onUsernameSubmitted event handler
+    // When this is called, we send POST request to /users route defined
+    return <UsernameForm onSubmit={this.onUsernameSubmitted} />
   }
 }
 
